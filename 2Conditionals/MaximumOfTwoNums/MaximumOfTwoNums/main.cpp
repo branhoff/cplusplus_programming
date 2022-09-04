009981418876c4450d2e6de75e00f39917d51140
branhoff <branhoff2@gmail.com>
2022-09-04 10:07:17 -0700

    first commit for  projects

---
+++ 2Conditionals/MaximumOfTwoNums/MaximumOfTwoNums/main.cpp
@@ -0,0 +1,34 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int x, y;
+    
+    std::cout << "Enter two nums (int): " << std::endl;
+
+    std::cout << "Num1: "; std::cin >> x;
+    std::cout << "Num2: "; std::cin >> y;
+
+    if (x > y) {
+        std::cout << "Max is " << x;
+    }
+    else {
+        std::cout << "Max is " << y;
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
